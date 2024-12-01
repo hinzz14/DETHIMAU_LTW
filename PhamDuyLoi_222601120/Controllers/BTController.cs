a9bb6595c255cd9fd2ba8b4aebf0947996dd0de2
hinzz14 <taoeopiet2004@gmail.com>
2024-12-02 02:23:33 +0700

    Add project files.

---
+++ PhamDuyLoi_222601120/Controllers/BTController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhamDuyLoi_222601120.Controllers
+{
+    public class BTController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
